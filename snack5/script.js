9715a7311432928f33e47bcc0aa9842c8fa22600
carloDM <carlo.de.mauro.work@gmail.com>
2023-03-31 17:21:23 +0200

    complete

---
+++ snack5/script.js
@@ -0,0 +1,57 @@
+console.log('ciao');
+
+// ------------------------------------------
+
+// const arrayDispari = [];
+
+// for (let i = 0; i < 6; i++) {
+//   const nuovoNumero = parseInt(prompt('inserisci numero')) ;
+
+//   if (nuovoNumero % 2 !== 0 ) {
+//     arrayDispari.push(nuovoNumero)
+//   }
+// }
+
+// console.log (arrayDispari);
+
+// ---------------------------------------------------------
+// ---------------------------------------------------------
+
+// const limite = 5;
+
+// // Math.pow(2 , 2) eleva alla potenza
+
+// for (let i = 1; i <limite; i++) {
+//   const cubo = Math.pow(i,3);
+//   console.log(cubo)
+// }
+
+// --------------------------------------------------------------
+// -------------------------------------------------------------
+
+// const limitePotenza = 1000;
+// let potenza = 1; 
+
+// while ( potenza < limitePotenza) {
+//   console.log(potenza);
+//   potenza *= 2;
+// }
+
+// --------------------------------------------------------------
+// -------------------------------------------------------------
+
+// const numeroUtente = prompt('num 4 cifre')
+// let sommaCifre = 0;
+
+// for (let i = 0; i < numeroUtente.length; i++) {
+//   // i caratteri di una styringa li posso prendere in base all indice come se fosse un array o con mia stringa.charAt(i)
+//   console.log(numeroUtente[i]);
+//   sommaCifre += parseInt(numeroUtente[i]); 
+// }
+
+// console.log(sommaCifre);
+
+// --------------------------------------------------------------
+// -------------------------------------------------------------
+
+
